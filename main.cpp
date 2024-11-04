c22a8734ef7f04324c74bf9a8be76cf0f8b19ca8
Joey Dhillon <imjoeydhillon@gmail.com>
2024-11-03 18:59:43 -0800

    Insert test added

--- main.cpp
+++ main.cpp
@@ -5,17 +5,19 @@
 #include <list>
 #include <vector>
 
+#include <algorithm>
+
 #include <chrono>
 #include <typeinfo>
 
 using namespace std;
 using namespace std::chrono;
 
-/// @brief Reads data into a vector and returns the time it took for the calculation
+/// @brief Reads data into a container that had working pushback and returns the time it took for the calculation
 /// @param data an empty vector
 /// @return a duration object containing time for caclulation
-milliseconds readIntoVector(vector<string> &data){
-    milliseconds fail;
+microseconds fillUsingPushBack(auto &data){
+    microseconds fail;
     auto start = high_resolution_clock::now();
 
     ifstream fin;
@@ -32,42 +34,16 @@ milliseconds readIntoVector(vector<string> &data){
     }
 
     auto end = high_resolution_clock::now();
-    milliseconds duration = duration_cast<milliseconds>(end - start);
+    microseconds duration = duration_cast<microseconds>(end - start);
 
     return duration;
 }
-
-/// @brief Reads data into a List and returns the time it took for the calculation
-/// @param data: an empty list
-/// @return a duration object containing time for caclulation
-milliseconds readIntoList(list<string> &data){
-    milliseconds fail;
-    auto start = high_resolution_clock::now();
-
-    ifstream fin;
-    fin.open("codes.txt");
-    if(!fin){
-        cout << "File opening error" << endl;
-        return fail;
-    }
-
-    while(fin){
-        string code;
-        getline(fin,code);
-        data.push_back(code);
-    }
-
-    auto end = high_resolution_clock::now();
-    milliseconds duration = duration_cast<milliseconds>(end - start);
-
-    return duration;
-}
-
 /// @brief Reads data into a set and returns the time it took for the calculation
 /// @param data an empty list
 /// @return a duration object containing time for caclulation
-milliseconds readIntoSet(set<string> &data){
-    milliseconds fail;
+microseconds fillUsingInsert(auto &data){
+
+    microseconds fail;
     auto start = high_resolution_clock::now();
 
     ifstream fin;
@@ -84,8 +60,45 @@ milliseconds readIntoSet(set<string> &data){
     }
 
     auto end = high_resolution_clock::now();
-    milliseconds duration = duration_cast<milliseconds>(end - start);
+    microseconds duration = duration_cast<microseconds>(end - start);
 
+    return duration;
+}
+
+microseconds sortVec(vector<string> &data){
+    auto start = high_resolution_clock::now();
+    sort(data.begin(),data.end());
+    auto end = high_resolution_clock::now();
+    microseconds duration = duration_cast<microseconds>(end-start);
+
+    return duration;
+}
+microseconds sortList(list<string> &data){
+    auto start = high_resolution_clock::now();
+    data.sort();
+    auto end = high_resolution_clock::now();
+    microseconds duration = duration_cast<microseconds>(end-start);
+    return duration;
+}
+
+microseconds insertMid(auto &data, string element){
+    auto middle = data.begin();
+    advance(middle, data.size()/2);
+
+    time_point start = steady_clock::now();
+    data.insert(middle, element);
+    time_point end = steady_clock::now();
+
+    microseconds duration = duration_cast<microseconds>(end - start);
+    return duration;
+}
+
+microseconds insertSet(set<string> &data, string element){
+    time_point start = steady_clock::now();
+    data.insert(element);
+    time_point end = steady_clock::now();
+
+    microseconds duration = duration_cast<microseconds> (end - start);
     return duration;
 }
 
@@ -94,9 +107,23 @@ int main() {
     list<string> li;
     set<string> set;
 
-    cout << readIntoVector(vec).count() << endl;
-    cout << readIntoList(li).count() << endl;
-    cout << readIntoSet(set).count() << endl;
+    cout << fillUsingPushBack(vec).count() << endl;
+    cout << fillUsingPushBack(li).count() << endl;
+    cout << fillUsingInsert(set).count() << endl;
+
+    cout << endl;
+
+    cout << sortVec(vec).count() << endl;
+    cout << sortList(li).count() << endl;
+    cout << -1 << endl;
+
+    cout << endl;
+
+    cout << insertMid(vec, "TESTCODE").count() << endl;
+    cout << insertMid(li, "TESTCODE").count() << endl;
+    cout << insertSet(set, "TESTCODE").count() << endl;
+    cout << vec.at(vec.size()/2-1) << endl;
+    
 
     return 0;
 }
@@ -104,6 +131,6 @@ int main() {
 /* syntax examples:
 auto start = high_resolution_clock::now()
 auto end = high_resolution_clock::now()
-auto duration = duration_cast<milliseconds>(end - start)
-duration.count() references elapsed milliseconds
+auto duration = duration_cast<microseconds>(end - start)
+duration.count() references elapsed microseconds
 */
